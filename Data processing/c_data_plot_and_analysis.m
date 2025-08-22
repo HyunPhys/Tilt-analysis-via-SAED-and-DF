df112f413b714772563c1c2845c01108af1f10d0
HyunPhys <125445318+HyunPhys@users.noreply.github.com>
2025-08-22 16:29:24 +0900

    Update c_data_plot_and_analysis.m
    Marker 등 color 선택하는 옵션 추가됨

--- Data processing/c_data_plot_and_analysis.m
+++ Data processing/c_data_plot_and_analysis.m
@@ -13,20 +13,30 @@ cd 'raw data'
 % filename_the_1 = "mean_and_std_right.xlsx";
 % filename_the_2 = "mean_and_std_right.xlsx";
 
-load("mean_and_std_250822_try_1.mat")
+load("mean_and_std_250807.mat")
 
 
 % 관심있는 polygon 선택
 polystart = 1;
-polyend = 2;
+polyend = 4;
 
 intensity_mean_of_interest = intensity_mean(1:end,polystart:polyend);
 intensity_std_of_interest = intensity_std(1:end,polystart:polyend);
 
 
 % Plot 관련 설저
-color_list = {"r","g","b","c","m","y","k"};
-legend_list = {'Red','Green'}; % ROI 이름을 string으로 넣기...
+% MarkerFace_list = {"#E67B99","#99CCB3","#BB8FCE","#6699CC"};
+% MarkerEdge_list = {"#8A4A5C","#5C7A6B","#70567B","#2A4D6F"};
+% color_list = {"#B8637A", "#7AA38F", "#9672A5", "#4072A0"};
+% MarkerFace_list = {"#FF4477", "#44CCAA", "#AA66FF", "#4477FF"};
+% MarkerEdge_list = {"#CC3366", "#33AA88", "#8844CC", "#3344CC"};
+% color_list      = {"#FF3366", "#33CC99", "#9966FF", "#3366FF"};
+MarkerFace_list = {"#E86880", "#63C4B5","#4C85E5", "#A976D7"};
+MarkerEdge_list = {"#B8475C", "#3B8F7F","#2A5FBF", "#714C9E"};
+color_list      = {"#D2526B", "#47B3A4","#467DE3", "#8E5ECF"};
+
+
+legend_list = {'A','B','C','D'}; % ROI 이름을 string으로 넣기...
 
 
 % 각도, 평균, 표준편차의 열 번호
@@ -68,9 +78,11 @@ end
 % 실험값 graph 그리기
 
 
-
+figure()
 
 hold on
+
+fontsize(14,"points")
 
 %plot(exp_data_1(:,1), exp_data_1(:,2),'marker','.', 'MarkerSize',7,'Color','r','LineStyle','none')
 %plot(exp_data_2(:,1), exp_data_2(:,2),'marker','.', 'MarkerSize',7,'Color','g','LineStyle','none')
@@ -86,7 +98,10 @@ hold on
 % e4=errorbar(a,matrix_D_region,div_D,div_D,'-s','MarkerSize',3,'MarkerEdgeColor','black','MarkerFaceColor','black','CapSize',1, 'color', 'black');
 
 for p=1:(polyend-polystart+1)
-    errorbar(FileNameArray,intensity_mean_of_interest(:,p),intensity_std_of_interest(:,p),'-s','MarkerSize',3,'MarkerEdgeColor',color_list{p},'MarkerFaceColor',color_list{p},'CapSize',1, 'Color', color_list{p})
+    errorbar(FileNameArray,intensity_mean_of_interest(:,p),intensity_std_of_interest(:,p), ...
+        '-o','MarkerSize',4,'MarkerEdgeColor',MarkerEdge_list{p}, ...
+        'MarkerFaceColor',MarkerFace_list{p},'CapSize',3, 'Color', color_list{p}, ...
+        'LineWidth',1)
 end
 
 
@@ -117,12 +132,12 @@ end
 % legend 등
 
 
-legend(legend_list, 'fontsize',7)
+legend(legend_list,'Location','north')
 
 
-xlabel('Tilt Angle [deg]','FontSize',10)
-ylabel('Normalized Intensity [a.u.]','FontSize',10)
-ylim([0,1])
+xlabel('Tilt Angle [deg]')
+ylabel('Normalized Intensity [a.u.]')
+ylim([-0.05,1.1])
 xlim([-20, 20])
 
 grid on
